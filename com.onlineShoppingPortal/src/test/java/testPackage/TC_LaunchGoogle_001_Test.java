75afb28ae6880b12ab0458997cbeddd1431729fa
ishac <ishac@LAPTOP-B8LU93MH>
2025-03-21 12:10:27 +0530

    Second Commit Version 0.2

---
+++ com.onlineShoppingPortal/src/test/java/testPackage/TC_LaunchGoogle_001_Test.java
@@ -0,0 +1,20 @@
+package testPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_LaunchGoogle_001_Test {
+  @Test
+  public void launchGoogle() throws InterruptedException 
+  {
+	  WebDriver driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	  driver.get("https://www.google.com/");
+	  Thread.sleep(3000);
+	  driver.quit();
+  }
+}
